48d12c02ba2d67c29848aa21724774cad674b215
roks0n <haluzan.rok@gmail.com>
2018-05-03 20:59:49 +0200

    increase cache from 5s to 5min :facepalm:

--- src/app/views/home.py
+++ src/app/views/home.py
@@ -25,7 +25,7 @@ class Homepage(TemplateView):
         if not delegates_list:
             delegates = Delegate.objects.raw(sql_delegates)  # todo: optimize this raw sql yo
             delegates_list = list(delegates)
-            cache.set('app.sql.get_delegates', delegates_list, 5)  # expire cache in 5min
+            cache.set('app.sql.get_delegates', delegates_list, 5 * 60)  # expire cache in 5min
 
         page = int(self.request.GET.get('page', 1))
         paginator = Paginator(delegates_list, 60)
