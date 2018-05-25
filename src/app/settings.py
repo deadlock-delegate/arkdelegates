e2f457a7fe8d05f4cb4b69914a5ba76b3bc676e8
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2018-05-25 16:23:02 +0200

    Added more lint checks

--- src/app/settings.py
+++ src/app/settings.py
@@ -82,11 +82,11 @@ TEMPLATES = [
 ]
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/0",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
