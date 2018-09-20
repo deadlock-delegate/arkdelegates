368a024c1ecf0fdf076961b18d22d01f88677ad9
Rok <haluzan.rok@gmail.com>
2018-09-20 17:06:21 +0200

    use whitenoise 3.3.1 - 4.1 seems to be broken (#36)

--- src/app/wsgi.py
+++ src/app/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for a project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
 from django.core.wsgi import get_wsgi_application
 
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
