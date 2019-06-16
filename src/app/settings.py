c53d3f43920e45aaa0d0bc3a0d1c99936eae59f5
roks0n <haluzan.rok@gmail.com>
2019-06-16 19:45:26 +0200

    use knox's serializer when outputing token data

--- src/app/settings.py
+++ src/app/settings.py
@@ -121,7 +121,7 @@ REST_KNOX = {
     "SECURE_HASH_ALGORITHM": "cryptography.hazmat.primitives.hashes.SHA512",
     "AUTH_TOKEN_CHARACTER_LENGTH": 64,
     "TOKEN_TTL": timedelta(hours=3),
-    "USER_SERIALIZER": "app.serializers.CustomAuthTokenSerializer",
+    "USER_SERIALIZER": "knox.serializers.UserSerializer",
     "TOKEN_LIMIT_PER_USER": None,
     "AUTO_REFRESH": False,
 }
